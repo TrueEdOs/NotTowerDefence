cf65a1cf6781e07ea2d0c2ea5ed15ea135ba76c1
mivladi99@mail.ru <mivladi99@mail.ru>
2019-05-14 00:14:14 +0300

    add menu

--- NoTowerDefence.py
+++ NoTowerDefence.py
@@ -22,10 +22,6 @@ runnable_stack = RunnableStack()
 runnable_stack.push(MainMenu(W, H, runnable_stack, color.brick))
 
 while not runnable_stack.is_empty():
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
     pygame.display.update()
     win.blit(runnable_stack.last_task().surface, (0, 0))
     runnable_stack.last_task().step()
